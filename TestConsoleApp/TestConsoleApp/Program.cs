10be2698d53ff2dd28cf1f6c3e85ff703de91e7b
Micael Nunes Loureiro <micaelnl@hotmail.com>
2019-04-15 11:21:01 +0100

    Primeiro Commit, Primeiro programa para o GitHub

---
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de uso GitHub");
+            Console.ReadKey();
+        
+        }
+    }
+}
